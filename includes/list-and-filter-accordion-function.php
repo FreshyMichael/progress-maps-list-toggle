3f0508b26fbd44c926556834a3ef39166e429818
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2021-08-31 09:52:05 -0400

    Update list-and-filter-accordion-function.php

--- includes/list-and-filter-accordion-function.php
+++ includes/list-and-filter-accordion-function.php
@@ -1,1 +1,9 @@
-
+(function($){
+		function $listAccordions(){
+		$('.cspml_details_title').click(function(event){
+			$(this).next('.cspml_details_content').toggle(event).preventDefault();
+		});
+}
+$(document).ready($listAccordions);
+$(document).ajaxComplete($listAccordions);
+})(jQuery);
